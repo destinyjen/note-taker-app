855ede12e7ec6465f5c45d039a33f4d127652b07
Destiny <dejenkins@justworks.com>
2024-06-30 18:18:33 -0400

    added initial server peices and middleware

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require ("express");
+
+const fs = require ("fs");
+
+const path = require ("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
