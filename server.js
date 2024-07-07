449aba316beb4705b11ad30f31964c26c663c0a6
Destiny <dejenkins@justworks.com>
2024-07-07 17:30:08 -0400

    middleware additions

--- server.js
+++ server.js
@@ -8,12 +8,29 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Middleware additions
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json",(err,data) => {
+        if (err) {console.log(err)}
+        const notes = JSON.parse(data)
+        console.log(notes)
+        res.json(notes)
+    })
+})
+// Add post route here, use unique id
+app.post('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json",(err,data) => {
+        if (err) {console.log(err)}
+        const notes = JSON.parse(data)
+        console.log(notes)
+        console.log(req.body)
+    })
+})
 
 
 app.get('/', (req, res) =>
